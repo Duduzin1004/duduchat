c959294cf7851459553d23d07151f4e906791183
Duduzin1004 <cruz.rocha.eduardo@escola.pr.gov.br>
2026-06-17 21:55:29 -0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,13 @@
+const userInfo = document.getElementById("userInfo");
+const friendsList = document.getElementById("friendsList");
+
+userInfo.innerHTML = `
+<h3>Eduardo</h3>
+<p>ID: 843291</p>
+`;
+
+friendsList.innerHTML = `
+<div class="friend">
+    Eloise
+</div>
+`;
