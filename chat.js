05451524066719349906d7549db0637ac1c90373
Duduzin1004 <cruz.rocha.eduardo@escola.pr.gov.br>
2026-06-18 20:03:10 -0300

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,13 +1,62 @@
+import { auth, db } from "./firebase.js";
+
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+import {
+  doc,
+  getDoc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
 const userInfo = document.getElementById("userInfo");
-const friendsList = document.getElementById("friendsList");
 
-userInfo.innerHTML = `
-<h3>Eduardo</h3>
-<p>ID: 843291</p>
-`;
+function gerarID() {
+  return Math.floor(
+    100000 + Math.random() * 900000
+  ).toString();
+}
 
-friendsList.innerHTML = `
-<div class="friend">
-    Eloise
-</div>
-`;
+onAuthStateChanged(auth, async (user) => {
+
+  if (!user) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  const userRef = doc(db, "users", user.uid);
+
+  const userSnap = await getDoc(userRef);
+
+  if (!userSnap.exists()) {
+
+    await setDoc(userRef, {
+      nome: user.email.split("@")[0],
+      email: user.email,
+      id: gerarID()
+    });
+
+  }
+
+  const dados = (await getDoc(userRef)).data();
+
+  userInfo.innerHTML = `
+    <h3>${dados.nome}</h3>
+    <p>ID: ${dados.id}</p>
+    <br>
+    <button id="logoutBtn">Sair</button>
+  `;
+
+  document
+    .getElementById("logoutBtn")
+    .addEventListener("click", async () => {
+
+      await signOut(auth);
+
+      window.location.href = "index.html";
+
+    });
+
+});
