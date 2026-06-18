0783a6ac76424196842f838b7c9b34fa721b4fea
Duduzin1004 <cruz.rocha.eduardo@escola.pr.gov.br>
2026-06-18 20:24:20 -0300

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,8 +1,34 @@
-console.log("CHAT FUNCIONANDO");
+import { auth } from "./firebase.js";
+
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
 const userInfo = document.getElementById("userInfo");
 
-userInfo.innerHTML = `
-    <h3>Eduardo</h3>
-    <p>ID: 843291</p>
-`;
+onAuthStateChanged(auth, (user) => {
+
+  if (!user) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  const nome = user.email.split("@")[0];
+
+  userInfo.innerHTML = `
+    <h3>${nome}</h3>
+    <button id="logoutBtn">Sair</button>
+  `;
+
+  document
+    .getElementById("logoutBtn")
+    .addEventListener("click", async () => {
+
+      await signOut(auth);
+
+      window.location.href = "index.html";
+
+    });
+
+});
