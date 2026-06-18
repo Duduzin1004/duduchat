cdd0db0e998962bc503969cc8eacf6052b79bab3
Duduzin1004 <cruz.rocha.eduardo@escola.pr.gov.br>
2026-06-17 21:27:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+import { auth } from "./firebase.js";
+
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  const erro = document.getElementById("erro");
+
+  try {
+
+    await signInWithEmailAndPassword(
+      auth,
+      email,
+      senha
+    );
+
+    window.location.href = "chat.html";
+
+  } catch (e) {
+
+    erro.textContent = "Email ou senha incorretos";
+
+    console.error(e);
+
+  }
+
+});
---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Importações Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+// Configuração do seu Firebase
+const firebaseConfig = {
+  apiKey: "SAIzaSyDQ1Aj_r9d3E26aky8R3wb1c5YtlDC5LS4",
+  authDomain: "duduchat-43b42.firebaseapp.com",
+  projectId: "duduchat-43b42",
+  storageBucket: "duduchat-43b42.firebasestorage.app",
+  messagingSenderId: "658458417869",
+  appId: "1:658458417869:web:c884e7666e138130a7e784"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
